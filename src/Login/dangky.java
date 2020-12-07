330d03a358049db4c51d71902d073a2bcb23be3c
An Nguyen <ducan172002@gmail.com>
2020-12-08 02:30:56 +0700

    update_json

--- src/Login/dangky.java
+++ src/Login/dangky.java
@@ -108,6 +108,14 @@ public class dangky {
         public void setPassword(String password) {
             this.password = password;
         }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
     }
 
 }
